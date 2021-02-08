cf0f7467b0a49528dd2108e4dadc9f34796a3427
s1d0r <sidorenko.v.n@gmail.com>
2021-02-08 22:18:36 +0200

    library project created

---
+++ src/BtcTradeUa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using s1d0r.marketplace;
+
+namespace s1d0r.btctradecomua
+{
+    public class BtcTradeUa : IMarketPlace
+    {
+        public string GetBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTiker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlaseBuyOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlaseSellOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool StatusOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
