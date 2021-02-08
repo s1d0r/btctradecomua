39082f8b193f7724cfa81b3631f055ebf8170ed6
s1d0r <sidorenko.v.n@gmail.com>
2021-02-08 22:58:48 +0200

    trying make POST request in GetTradingBalance medhod

--- src/BtcTradeUa.cs
+++ src/BtcTradeUa.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using s1d0r.marketplace;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace s1d0r.btctradecomua
 {
     public class BtcTradeUa : IMarketPlace
     {
-        public string GetBalance()
+        private int outOrderId;
+        private int nonce;
+
+        public BtcTradeUa(HttpClient httpClient, String publicKey)
+        {
+            HttpClient = httpClient;
+            PublicKey = publicKey;
+        }
+
+        public HttpClient HttpClient { get; }
+        public string PublicKey { get; }
+
+        /// <summary>
+        /// https://btc-trade.com.ua/api/balance
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetBalanceAsync()
+        {
+            using (var request = new HttpRequestMessage(
+                HttpMethod.Post,
+                new Uri("https://btc-trade.com.ua/api/balance")
+                ))
+            {
+                request.Headers.Authorization = SetAuthenticationHeader(PublicKey);
+
+                var data = new Dictionary<string, string>();
+                data.Add("out_order_id", outOrderId.ToString());
+                data.Add("nonce", nonce.ToString());
+                request.Content = new FormUrlEncodedContent(data);
+
+                using (var response = await HttpClient.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                        return await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+                    else
+                        return null;
+                }
+            }
+        }
+
+        private AuthenticationHeaderValue SetAuthenticationHeader(string publicKey)
         {
             throw new NotImplementedException();
         }
