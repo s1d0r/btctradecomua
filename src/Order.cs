2719f8a7868e11ded9073d61d771a25b317da17d
s1d0r <sidorenko.v.n@gmail.com>
2021-04-28 14:04:05 +0300

    added class fields

---
+++ src/Order.cs
@@ -0,0 +1,15 @@
+namespace s1d0r.btctradecomua
+{
+    public class Order
+    {
+        public string type;
+        public int id;
+        public float price;
+        public float sum1;
+        public float sum2;
+        public float amnt_trade;
+        public int unixtime;
+        public string pub_date;
+        public float amnt_base;
+    }
+}
---
+++ src/Orders.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace s1d0r.btctradecomua
+{
+    public class Orders
+    {
+        public bool status;
+        public bool auth;
+
+        public List<Order> your_open_orders;
+    }
+}
